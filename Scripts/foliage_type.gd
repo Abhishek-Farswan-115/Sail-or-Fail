981b0f48f5be049155a052d982a306f07642a498
dmoraleda11@gmail.com <dmoraleda11@gmail.com>
2023-02-10 16:09:59 +0100

    Changed the generation algorithm to spawn foliage from a foliage resource.

---
+++ Scripts/foliage_type.gd
@@ -0,0 +1,5 @@
+class_name FoliageType extends Resource
+
+@export_category("Parameters")
+@export var instance_scene: PackedScene
+@export var instance_number: int
--- Scripts/terrain_block.gd
+++ Scripts/terrain_block.gd
@@ -4,8 +4,7 @@ class_name TerrainBlock extends Node3D
 @export var size: Vector3 = Vector3.ONE
 
 @export_category("Decorations")
-@export var decoration_amount: int = 10
-@export var decoration_scenes: Array[PackedScene]
+@export var foliage_types: Array[FoliageType]
 
 @onready var editor_nodes: Node3D = $decorations/editor
 
@@ -14,14 +13,15 @@ func _ready() -> void:
 	editor_nodes.visible = false
 
 func spawn_decoration() -> void:
-	if decoration_scenes.is_empty(): return
+	if foliage_types.is_empty(): return
 	
-	var rate := int(decoration_amount / editor_nodes.get_child_count())	
-	for i in editor_nodes.get_children():
-		if i is MeshInstance3D and i.mesh is PlaneMesh:
-			for j in range(rate):
-				var dec: Node3D = decoration_scenes[randi() % decoration_scenes.size()].instantiate()
-				$decorations.add_child(dec)
-				dec.position = Vector3(randf_range(-i.mesh.size.x/2, i.mesh.size.x/2), 0.0, randf_range(-i.mesh.size.y/2, i.mesh.size.y/2))
-				dec.position += i.position
-				dec.rotation.y = randf_range(-PI, PI)
+	for fol in foliage_types:
+		var rate := int(fol.instance_number / editor_nodes.get_child_count())
+		for i in editor_nodes.get_children():
+			if i is MeshInstance3D and i.mesh is PlaneMesh:
+				for j in range(rate):
+					var dec: Node3D = foliage_types[randi() % foliage_types.size()].instance_scene.instantiate()
+					$decorations.add_child(dec)
+					dec.position = Vector3(randf_range(-i.mesh.size.x/2, i.mesh.size.x/2), 0.0, randf_range(-i.mesh.size.y/2, i.mesh.size.y/2))
+					dec.position += i.position
+					dec.rotation.y = randf_range(-PI, PI)
